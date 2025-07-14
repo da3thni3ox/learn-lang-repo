1ba831d9a271f0c235636661b3d14e69760229c1
darthNitrox <darthnitrox@gmail.com>
2025-07-15 00:53:36 +0500

    Mltable

---
+++ algo/mltable/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var mltable [][]int
+	var tempMlTable []int
+
+	size := 2
+	for i := 1; i <= size; i++ {
+		tempMlTable = make([]int, 3, 3)
+		for j := 1; j <= size; j++ {
+			res := i * j
+			tempMlTable[j-1] = res
+		}
+
+		mltable = append(mltable, tempMlTable)
+	}
+
+	fmt.Println(mltable)
+}
