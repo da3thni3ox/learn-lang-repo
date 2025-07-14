c1035af9bd4b31bc3b016889d924a6b9bf070c9d
darthNitrox <darthnitrox@gmail.com>
2025-07-15 00:59:51 +0500

    Fix bug

--- algo/mltable/main.go
+++ algo/mltable/main.go
@@ -6,9 +6,9 @@ func main() {
 	var mltable [][]int
 	var tempMlTable []int
 
-	size := 2
+	size := 9
 	for i := 1; i <= size; i++ {
-		tempMlTable = make([]int, 3, 3)
+		tempMlTable = make([]int, size)
 		for j := 1; j <= size; j++ {
 			res := i * j
 			tempMlTable[j-1] = res
